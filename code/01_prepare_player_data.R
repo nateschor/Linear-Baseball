18f2f16a8e2b78d99d096229169d959e94fbbc26
Nathan Schor <nate.schor@gmail.com>
2022-08-14 17:52:27 -0400

    Add player's team and merge datasets #4

--- code/01_prepare_player_data.R
+++ code/01_prepare_player_data.R
@@ -8,6 +8,9 @@ df_2021 <- read_fst("data/raw/all_data.fst") %>%
   filter(year_ID == 2021) %>% 
   glimpse()
 
+
+# Salary ------------------------------------------------------------------
+
 df_salary <- df_2021 %>% 
   group_by(player_name) %>% 
   summarize(
@@ -18,6 +21,8 @@ df_salary <- df_2021 %>%
     if_all(c(salary, bwar), ~ !is.na(.))
   ) %>% 
   print()
+
+# Position ---------------------------------------------------------------
 
 df_pitcher_position <- df_2021 %>% 
   select(player_name, starts, g_pitch) %>% 
@@ -62,5 +67,28 @@ check_unique_players <- df_position %>%
 
 stopifnot("Each player should only occur once in dataset" = check_unique_players == 1)
 
+# Team --------------------------------------------------------------------
 
+df_team <- df_2021 %>% 
+  select(player_name, team_ID, stint_ID, starts_with("gms")) %>% 
+  pivot_longer(., starts_with("gms"), 
+               names_to = "position", values_to = "games", names_prefix = "gms_") %>% 
+  select(-position) %>% 
+  group_by(player_name, team_ID, stint_ID) %>% 
+  summarize(
+    games = sum(games)
+  ) %>% 
+  group_by(player_name) %>% 
+  slice_max(games, with_ties = FALSE) %>% 
+  ungroup() %>% 
+  select(player_name, team = team_ID)
+
+
+# Merged Dataset ----------------------------------------------------------
+
+df <- list(df_salary, df_position, df_team) %>% 
+  reduce(full_join, by = "player_name") %>% 
+  filter(
+    if_all(everything(), ~ !is.na(.))
+  )
 
